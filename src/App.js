8e7ab02b5512206d13c7dfedda8f3e1701024b90
ehmusman <ehmusman@gmail.com>
2020-11-13 23:24:43 +0500

    Read and Updated is completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Todo from './components/Todo';
 import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
 
 function App() {
-  const [todos] = useState([
+  const [todos, setTodos] = useState([
     {
       text: "learn about react",
       inCompleted: true
@@ -17,7 +18,12 @@ function App() {
       text: "Building really cool react apps",
       inCompleted: true
     }
-  ])
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
   return (
     <div className="bg-primary pt-5" style={{ height: "100vh" }}>
       <div className="container">
@@ -30,6 +36,9 @@ function App() {
             todo={todo}
           />
         ))}
+        <TodoForm
+          addTodo={addTodo}
+        />
       </div>
     </div>
   );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (value === '') {
+            return;
+        };
+        addTodo(value);
+        setValue("");
+
+    }
     return (
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input type="text" className=" w-100 py-3 h4" placeholder="Add Todo...."
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
+            </form>
         </div>
     )
 }
